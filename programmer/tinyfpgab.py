1dce83fcb2d454badb56e8bc6a4b2a5ea0300e5c
Rogdham <contact@rogdham.net>
2017-10-28 13:42:46 +0200

    programmer: PEP8

--- programmer/tinyfpgab.py
+++ programmer/tinyfpgab.py
@@ -2,12 +2,13 @@
 
 import array
 
+
 def h(a):
     return ' '.join("%02X" % i for i in a)
 
 
 class TinyFPGAB(object):
-    def __init__(self, ser, progress = None):
+    def __init__(self, ser, progress=None):
         self.ser = ser
         self.spinner = 0
 
@@ -30,7 +31,6 @@ class TinyFPGAB(object):
                 time.sleep(0.05)
         return False
 
-
     def cmd(self, write_data, read_len):
         write_length_lo = len(write_data) & 0xFF
         write_length_hi = (len(write_data) >> 8) & 0xFF
@@ -39,32 +39,34 @@ class TinyFPGAB(object):
             cmd_read_len = read_len + 1
         read_length_lo = cmd_read_len & 0xFF
         read_length_hi = (cmd_read_len >> 8) & 0xFF
-        cmd_write_string = array.array('B', [1, write_length_lo, write_length_hi, read_length_lo, read_length_hi] + list(write_data)).tostring()
+        cmd_write_string = array.array('B', [
+            1, write_length_lo, write_length_hi,
+            read_length_lo, read_length_hi] + list(write_data)
+        ).tostring()
         self.ser.write(cmd_write_string)
         self.ser.flush()
         cmd_read_string = self.ser.read(read_len)
         return array.array('B', cmd_read_string).tolist()
 
-
     def sleep(self):
         self.cmd([0xB9], 0)
-
 
     def wake(self):
         self.cmd([0xAB], 0)
 
-
     def read_id(self):
         return self.cmd([0x9F], 3)
-
 
     def read_sts(self):
         return self.cmd([0x05], 1)
 
-
     def read(self, addr, length):
         if length <= 16:
-            read_value = self.cmd([0x0B, (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, addr & 0xFF, 0], length)
+            read_value = self.cmd(
+                [0x0B,
+                 (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, addr & 0xFF,
+                 0],
+                length)
             self.progress(length)
             return read_value
 
@@ -82,20 +84,15 @@ class TinyFPGAB(object):
 
             return return_data
 
-
     def write_enable(self):
         self.cmd([0x06], 0)
-
 
     def write_disable(self):
         self.cmd([0x04], 0)
 
-
     def wait_while_busy(self):
         while (self.read_sts()[0] & 1):
             pass
-
-
 
     def _erase(self, addr, length):
         if length == (4 * 1024):
@@ -109,27 +106,29 @@ class TinyFPGAB(object):
             opcode = 0xD8
 
         self.write_enable()
-        self.cmd([opcode, (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, addr & 0xFF], 0)
+        self.cmd(
+            [opcode,
+             (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, addr & 0xFF],
+            0)
         self.wait_while_busy()
-
-
 
     def erase(self, addr, length):
         possible_lengths = [1, 4 * 1024, 32 * 1024, 64 * 1024]
-        
+
         remaining_length = length
         current_addr = addr
 
         while remaining_length > 0:
-            erase_length = max([p for p in possible_lengths if p <= remaining_length and current_addr % p == 0])
-        
+            erase_length = max(
+                p for p in possible_lengths
+                if p <= remaining_length and current_addr % p == 0)
+
             if erase_length == 1:
                 start_addr = current_addr & 0xfff000
                 start_length = current_addr % (4 * 1024)
-                
-                
+
                 end_addr = (current_addr + remaining_length)
-                end_length = (4 * 1024) - (end_addr % (4 * 1024)) 
+                end_length = (4 * 1024) - (end_addr % (4 * 1024))
                 end_read_data = []
 
                 do_start = current_addr % (4 * 1024) != 0
@@ -137,14 +136,14 @@ class TinyFPGAB(object):
                 if do_start:
                     start_read_data = self.read(start_addr, start_length)
 
-                do_end = (current_addr & 0xfff000) == ((current_addr + remaining_length) & 0xfff000)
+                current_block_start = current_addr & 0xfff000
+                last_block_start = (current_addr + remaining_length) & 0xfff000
+                do_end = current_block_start == last_block_start
 
-                if do_end: 
-                    end_read_data = self.read(end_addr, end_length) 
+                if do_end:
+                    end_read_data = self.read(end_addr, end_length)
 
                 self._erase(start_addr, 4 * 1024)
-
-                
 
                 if do_start:
                     self.write(start_addr, start_read_data)
@@ -164,16 +163,20 @@ class TinyFPGAB(object):
                 remaining_length -= erase_length
                 current_addr += erase_length
 
-
     # don't use this directly, use the public "write" function instead
     def _write(self, addr, data):
         self.write_enable()
-        self.cmd([0x02, (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, addr & 0xFF] + list(data), 0)
+        self.cmd(
+            [0x02,
+             (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, addr & 0xFF
+             ] + list(data),
+            0)
 
-        # FIXME: this is a workaround for a bug in the bootloader verilog.  if the status
-        #        register read comes too early, then it corrupts the SPI flash write in
-        #        progress.  this busy loop waits long enough such that the write data has
-        #        finished and data corruption is no longer possible.
+        # FIXME: this is a workaround for a bug in the bootloader verilog.  if
+        #        the status register read comes too early, then it corrupts the
+        #        SPI flash write in progress.  this busy loop waits long enough
+        #        such that the write data has finished and data corruption is
+        #        no longer possible.
         import timeit
         t = timeit.default_timer()
         while timeit.default_timer() - t < 0.000060:
@@ -181,9 +184,6 @@ class TinyFPGAB(object):
 
         self.wait_while_busy()
         self.progress(len(data))
-
-
-
 
     def write(self, addr, data):
         remaining_length = len(data)
@@ -197,8 +197,6 @@ class TinyFPGAB(object):
             current_addr += write_length
             offset += write_length
             remaining_length -= write_length
-
-
 
     def program(self, addr, data):
         self.progress("Erasing designated flash pages")
@@ -223,19 +221,17 @@ class TinyFPGAB(object):
 
             return False
 
-
     def boot(self):
         self.ser.write("\x00")
         self.ser.flush()
 
-
     def slurp(self, filename):
         if filename.endswith(".hex"):
-            return (0x30000, [int(i, 16) for i in open(filename).read().split()])
+            return (0x30000, [int(i, 16)
+                              for i in open(filename).read().split()])
 
         if filename.endswith(".bin"):
             return (0x30000, list(bytearray(open(filename, 'rb').read())))
-
 
     def program_bitstream(self, addr, bitstream):
         self.progress("Waking up SPI flash")
@@ -249,7 +245,8 @@ class TinyFPGAB(object):
             self.boot()
             return True
 
-        # FIXME: printing out this spinner ensures the busy loop in _write isn't optimized away
+        # FIXME: printing out this spinner ensures the busy loop in _write is
+        #        not optimized away
         print "Your lucky number: " + str(self.spinner)
         return False
 
@@ -263,9 +260,13 @@ if __name__ == '__main__':
 
     p = argparse.ArgumentParser()
 
-    p.add_argument("-l", "--list", action="store_true", help="list connected and active TinyFPGA B-series boards")
-    p.add_argument("-p", "--program", type=str, help="program TinyFPGA board with the given bitstream")
-    p.add_argument("-b", "--boot", action="store_true", help="command the TinyFPGA B-series board to exit the bootloader and load the user configuration")
+    p.add_argument("-l", "--list", action="store_true",
+                   help="list connected and active TinyFPGA B-series boards")
+    p.add_argument("-p", "--program", type=str,
+                   help="program TinyFPGA board with the given bitstream")
+    p.add_argument("-b", "--boot", action="store_true",
+                   help="command the TinyFPGA B-series board to exit the "
+                        "bootloader and load the user configuration")
     p.add_argument("-c", "--com", type=str, help="serial port name")
 
     args = p.parse_args()
@@ -303,7 +304,8 @@ if __name__ == '__main__':
             if isinstance(info, str):
                 print "    " + info
 
-        with serial.Serial(active_port, 115200, timeout=0.2, writeTimeout=0.2) as ser:
+        with serial.Serial(active_port, 115200, timeout=0.2,
+                           writeTimeout=0.2) as ser:
             fpga = TinyFPGAB(ser, progress)
 
             (addr, bitstream) = fpga.slurp(args.program)
@@ -316,7 +318,7 @@ if __name__ == '__main__':
                 program_failure = True
                 traceback.print_exc()
 
-    if args.boot: 
+    if args.boot:
         if active_port is None:
             print "    No port was specified and no active bootloaders found."
             print "    Activate bootloader by pressing the reset button."
@@ -324,8 +326,7 @@ if __name__ == '__main__':
 
         print "    Booting " + active_port
 
-        with serial.Serial(active_port, 10000000, timeout=0.1, writeTimeout=0.1) as ser:
+        with serial.Serial(active_port, 10000000, timeout=0.1,
+                           writeTimeout=0.1) as ser:
             fpga = TinyFPGAB(ser, None)
             fpga.boot()
-
-
